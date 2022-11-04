61a209ee0260403f029e1f888ea3688e53c06bac
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-11-04 10:21:53 -0300

    task 1 done?

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('O primeiro link deve possuir o texto Home;', () => {
+  renderWithRouter(<App />);
+  const linkHome = screen.getByRole('link', { name: 'Home' });
+  const linkAbout = screen.getByRole('link', { name: 'About' });
+  const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+  expect(linkHome).toBeInTheDocument();
+  expect(linkAbout).toBeInTheDocument();
+  expect(linkFavorite).toBeInTheDocument();
+});
