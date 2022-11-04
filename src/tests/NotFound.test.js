dd7218b959c065ca6b82811406594367e299e04a
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-11-04 11:14:05 -0300

    task 4 done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('4) Teste o componente <NotFound.js />', () => {
+  const { history } = renderWithRouter(<App />);
+
+  act(() => {
+    history.push('/paginaProibida');
+  });
+  const { pathname } = history.location;
+  expect(pathname).toBe('/paginaProibida');
+
+  const pageNotFound = screen.getByText('Page requested not found');
+
+  expect(pageNotFound).toBeInTheDocument();
+
+  const altText = screen.getByAltText('Pikachu crying because the page requested was not found');
+
+  expect(altText).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
