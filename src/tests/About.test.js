42d5593b67ab1bc89d416a7bd4373760adcedc48
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-11-04 10:50:27 -0300

    task 2 done

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste se a página contém as informações sobre a Pokédex;', () => {
+  renderWithRouter(<App />);
+  const linkAbout = screen.getByRole('link', { name: 'About' });
+  userEvent.click(linkAbout);
+  const aboutPokedex = screen.getByRole('heading', { name: 'About Pokédex' });
+  const altPokedex = screen.getByAltText('Pokédex');
+
+  expect(aboutPokedex).toBeInTheDocument();
+  expect(altPokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
