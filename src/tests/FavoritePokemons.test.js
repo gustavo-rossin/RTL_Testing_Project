a7949c4b4fb2a9596306f9c487af82cf6f2f3e8b
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-11-04 10:56:01 -0300

    task 3 done

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha pokémons favoritos;', () => {
+  renderWithRouter(<App />);
+  const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+  userEvent.click(linkFavorite);
+  expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
+});
